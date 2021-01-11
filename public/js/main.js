39fb86f6ef3e76758ecb04096a961a4619250e15
Rafael Tognon <rafaelrovanitognon@gmail.com>
2021-01-11 16:19:56 -0300

    Projeto finalizado

---
+++ public/js/main.js
@@ -0,0 +1,91 @@
+var tempoInicial = $("#tempo-restante").text();
+var campo = $(".campo-digitacao");
+
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#reiniciar").click(reiniciaJogo);
+    atualizaPlacar();
+    $("#usuarios").selectize({
+        create: true,
+        sortField: 'text'
+    });
+    $(".tooltip").tooltipster({
+        trigger: "custom"
+    });
+});
+
+function atualizaTempoInicial(tempo) {
+    tempoInicial = tempo;
+    $("#tempo-restante").text(tempo);
+}
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input",function() {
+        var conteudo = campo.val();
+        
+        var qtdPalavras = conteudo.split(/\S+/).length-1;
+        $("#contador-palavras").text(qtdPalavras);
+        
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaMarcadores() {
+    campo.on("input",function() {
+        var frase = $(".frase").text();
+        var digitado = campo.val();
+        var comparavel = frase.substr(0,digitado.length);
+
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }
+        else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
+function inicializaCronometro() {
+    campo.one("focus", function(){
+        var tempoRestante = $("#tempo-restante").text();
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-restante").text(tempoRestante);
+            if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+            }
+        },1000);
+    });
+}
+
+function finalizaJogo () {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-restante").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
---
+++ servidor/app/api/frases.js
@@ -0,0 +1,27 @@
+var api = {};
+
+var frases = [
+	{_id: 0, texto:'Adolescente, adole, sente, a dor ele sente, sente na certa sem receio, teu cu na reta, meu pau sem freio', tempo: 15 },
+	{_id: 1, texto:'Debuggers não consertam erros, apenas os exibem em slow motion.',tempo: 8 },
+	{_id: 2, texto:'Siga seu sonho. Continue dormindo', tempo: 5 },
+	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
+	{_id: 4, texto:'Ciência da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
+	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
+	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
+	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
+	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
+	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
+
+	];
+
+api.lista = function(req, res) {
+
+	setTimeout(function(){
+		if(req.query.id) return res.json(frases[req.query.id]);
+
+		res.json(frases);
+	},1500);
+
+};
+
+module.exports = api;
